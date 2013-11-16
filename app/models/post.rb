55995878e9c14dfa6853e328e2dfffa86d1bc687
pdostal <git@pdostal.cz>
2013-11-16 04:12:06 +0100

    Comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ActiveRecord::Base
+  #attr_accessible :content, :name, :title
+   
+  validates :name,  :presence => true
+  validates :title, :presence => true,
+                    :length => { :minimum => 5 }
+ 
+  has_many :comments
 end
+
+#class Post < ActiveRecord::Base
+#end
